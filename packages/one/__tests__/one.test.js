0c654b7e6c3be292bd523b4ba8565114a3a46261
jun.yu <yujun@linklogis.com>
2022-11-28 15:50:52 +0800

    feat: first commit

---
+++ packages/one/__tests__/one.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const one = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(one(), 'Hello from one');
+console.info("one tests passed");
---
+++ packages/two/__tests__/two.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const two = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(two(), 'Hello from two');
+console.info("two tests passed");
